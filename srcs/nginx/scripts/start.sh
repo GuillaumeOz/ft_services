5ddf01e311e5fdcdabe36683abc54d00b4ea624c
guillaumeoz <guillaume.ozserttas@gmail.com>
2020-06-12 19:40:20 +0200

    Work on Nginx

--- srcs/nginx/scripts/start.sh
+++ srcs/nginx/scripts/start.sh
@@ -6,14 +6,20 @@
 #    By: gozsertt <gozsertt@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2020/06/11 17:15:09 by gozsertt          #+#    #+#              #
-#    Updated: 2020/06/11 17:16:27 by gozsertt         ###   ########.fr        #
+#    Updated: 2020/06/12 14:55:33 by gozsertt         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 #!/bin/sh
 
 adduser -D "$SSH_USER"
+# update passwords for the new user
 echo "$SSH_USER:$SSH_PASSWORD" | chpasswd
 
+# sshd (OpenSSH Daemon) is the daemon program for ssh
+# Together these programs replace rlogin and rsh, and provide secure
+# encrypted communications between two untrusted hosts over an insecure network.
 /usr/sbin/sshd
+# For normal production (on a server), use the default daemon on;
+# In this case for Docker containers (or for debugging), the daemon off;
 /usr/sbin/nginx -g 'daemon off;'
