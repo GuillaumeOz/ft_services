eff9ec5ba82128cfe7d30a5aabec04cb2cfb74db
user42 <gozsertt@student.42.fr>
2020-06-24 19:42:14 +0200

    Fix sql manifest

--- srcs/ftps/scripts/start-tmp.sh
+++ srcs/ftps/scripts/start-tmp.sh
@@ -16,4 +16,4 @@ mkdir -p /ftps/$FTP_USER
 adduser -h /ftps/$FTP_USER -D $FTP_USER
 echo "$FTP_USER:$FTP_PASSWORD" | chpasswd
 
-/usr/sbin/pure-ftpd -j -Y 2 -p 21000:21000 -P "MINIKUBE_IP"
+/usr/sbin/pure-ftpd -j -Y 2 -p 21000:21000 -P "172.17.0.2"
